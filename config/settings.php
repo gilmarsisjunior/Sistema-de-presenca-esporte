7633e1dd3898fddac84f59150e3bff32f9406cfc
Gilmar Silva Santos Junior <gilmar.sisjunior@gmail.com>
2021-04-21 20:00:45 -0300

    corrigido bug na importação de variaveis global

--- config/settings.php
+++ config/settings.php
@@ -1,11 +1,14 @@
 <?php
+session_start();
 
 
-if(isset($_POST['nome'])) {
+if(isset($_POST['nome'])){
     $nome = $_POST['nome'];
+    $_SESSION['nome'] = $nome;
     header('location: ../views/confirmar.php');
-    
-exit();
 }
+
+
+
 
 ?> 
